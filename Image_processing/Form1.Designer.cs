4f6f3a31acd1e274346b9744be01c2c25b0acd60
TiaYeah <71660852+TiaYeah@users.noreply.github.com>
2022-10-10 13:25:40 +0300

    base program

---
+++ Image_processing/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Image_processing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.фильтрToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayscaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.averageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autocontrastToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(629, 337);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.фильтрToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть ";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // фильтрToolStripMenuItem
+            // 
+            this.фильтрToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grayscaleToolStripMenuItem,
+            this.averageToolStripMenuItem,
+            this.autocontrastToolStripMenuItem});
+            this.фильтрToolStripMenuItem.Name = "фильтрToolStripMenuItem";
+            this.фильтрToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.фильтрToolStripMenuItem.Text = "Фильтр";
+            // 
+            // grayscaleToolStripMenuItem
+            // 
+            this.grayscaleToolStripMenuItem.Name = "grayscaleToolStripMenuItem";
+            this.grayscaleToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.grayscaleToolStripMenuItem.Text = "grayscale";
+            this.grayscaleToolStripMenuItem.Click += new System.EventHandler(this.grayscaleToolStripMenuItem_Click);
+            // 
+            // averageToolStripMenuItem
+            // 
+            this.averageToolStripMenuItem.Name = "averageToolStripMenuItem";
+            this.averageToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.averageToolStripMenuItem.Text = "average";
+            this.averageToolStripMenuItem.Click += new System.EventHandler(this.averageToolStripMenuItem_Click);
+            // 
+            // autocontrastToolStripMenuItem
+            // 
+            this.autocontrastToolStripMenuItem.Name = "autocontrastToolStripMenuItem";
+            this.autocontrastToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.autocontrastToolStripMenuItem.Text = "autocontrast";
+            this.autocontrastToolStripMenuItem.Click += new System.EventHandler(this.autocontrastToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 394);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ожидание";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem фильтрToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayscaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem averageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem autocontrastToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Image_processing/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_processing
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files | *.png; *.jpg; *.bmp; | All Files (*.*) | *.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                image = new Bitmap(dialog.FileName);
+                pictureBox1.Image = image;
+                pictureBox1.Refresh();
+            }
+        }
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveDialog.OpenFile();
+                switch (saveDialog.FilterIndex)
+                {
+                    case 1:
+                        image.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    case 2:
+                        image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case 3:
+                        image.Save(fs, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                }
+            }
+        }
+        private void grayscaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Обработка изображения...";
+            Bitmap resultImage = new Bitmap(image.Width, image.Height);
+            
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color sourceColor = image.GetPixel(i, j);
+                    int Intensity = (int)(0.299 * sourceColor.R + 0.587 * sourceColor.G + 0.114 * sourceColor.B);
+                    Color resultColor = Color.FromArgb(Intensity, Intensity, Intensity);
+                    resultImage.SetPixel(i, j, resultColor);
+                }
+            }
+            pictureBox1.Image = resultImage;
+            pictureBox1.Refresh();
+            image = resultImage;
+            label1.Text = "Обработка завершена";
+        }
+
+        private void averageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Обработка изображения...";
+            Bitmap resultImage = new Bitmap(image.Width, image.Height);
+            int radiusX = 1;
+            int radiusY = 1;
+
+            int red = 0, green = 0, blue = 0;
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    for (int l = -radiusY; l <= radiusY; l++)
+                    {
+                        for (int k = -radiusX; k <= radiusX; k++)
+                        {
+                            int idX = Clamp(i + k, 0, image.Width - 1);
+                            int idY = Clamp(j + l, 0, image.Height - 1);
+                            red += image.GetPixel(idX, idY).R;
+                            green += image.GetPixel(idX, idY).G;
+                            blue += image.GetPixel(idX, idY).B;
+                        }
+                    }
+                    Color resultColor = Color.FromArgb(Clamp(red / 9, 0, 255), Clamp(green / 9, 0, 255), Clamp(blue / 9, 0, 255));
+                    red = green = blue = 0;
+                    resultImage.SetPixel(i, j, resultColor);
+                }
+            }
+            pictureBox1.Image = resultImage;
+            pictureBox1.Refresh();
+            image = resultImage;
+            label1.Text = "Обработка завершена";
+        }
+
+        private int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+                return min;
+            if (value > max)
+                return max;
+            return value;
+        }
+
+        private void autocontrastToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Обработка изображения...";
+            Bitmap resultImage = new Bitmap(image.Width, image.Height);
+            double R2, G2, B2, R3, G3, B3;
+            R2 = maxR(image);
+            G2 = maxG(image);
+            B2 = maxB(image);
+            R3 = minR(image);
+            G3 = minG(image);
+            B3 = minB(image);
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color sourceColor = image.GetPixel(i, j);
+                    int R1 = sourceColor.R;
+                    int G1 = sourceColor.G;
+                    int B1 = sourceColor.B;
+
+                    int newR = (int)((R1 - R3) * 255 / (R2 - R3));
+                    int newG = (int)((G1 - G3) * 255 / (G2 - G3));
+                    int newB = (int)((B1 - B3) * 255 / (B2 - B3));
+
+
+                    Color resultColor = Color.FromArgb(Clamp(newR, 0, 255), Clamp(newG, 0, 255), Clamp(newB, 0, 255));
+                    resultImage.SetPixel(i, j, resultColor);
+                }
+            }
+            pictureBox1.Image = resultImage;
+            pictureBox1.Refresh();
+            image = resultImage;
+            label1.Text = "Обработка завершена";
+        }
+
+        public static double maxR(Bitmap image)
+        {
+            double R = -1;
+            Color imageColor;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    imageColor = image.GetPixel(i, j);
+                    if (R < imageColor.R)
+                        R = imageColor.R;
+                }
+            }
+            return R;
+        }
+        public static double maxG(Bitmap image)
+        {
+            double G = -1;
+            Color imageColor;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    imageColor = image.GetPixel(i, j);
+                    if (G < imageColor.G)
+                        G = imageColor.G;
+                }
+            }
+            return G;
+        }
+        public static double maxB(Bitmap image)
+        {
+            double B = -1;
+            Color imageColor;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    imageColor = image.GetPixel(i, j);
+                    if (B < imageColor.B)
+                        B = imageColor.B;
+                }
+            }
+            return B;
+        }
+
+        public static double minR(Bitmap image)
+        {
+            double R = 256;
+            Color imageColor;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    imageColor = image.GetPixel(i, j);
+                    if (R > imageColor.R)
+                        R = imageColor.R;
+                }
+            }
+            return R;
+        }
+        public static double minG(Bitmap image)
+        {
+            double G = 256;
+            Color imageColor;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    imageColor = image.GetPixel(i, j);
+                    if (G > imageColor.G)
+                        G = imageColor.G;
+                }
+            }
+            return G;
+        }
+        public static double minB(Bitmap image)
+        {
+            double B = 256;
+            Color imageColor;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    imageColor = image.GetPixel(i, j);
+                    if (B > imageColor.B)
+                        B = imageColor.B;
+                }
+            }
+            return B;
+        }
+
+        
+    }
+}
